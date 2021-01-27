ef99ed363f9ec0f7c8502e0cd0aa45cedf8ee5d8
Marc Kalil <marc@marckalil.com>
2021-01-27 15:59:23 +0100

    Firebase app setup

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {ReactNode} from 'react';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+import firebase from 'firebase';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = (): ReactNode => {
+  React.useEffect(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBrrQBzbzH5aA9ayAOaErgka9x8sqjwIzI',
+      authDomain: 'gittest-3c4d1.firebaseapp.com',
+      projectId: 'gittest-3c4d1',
+      storageBucket: 'gittest-3c4d1.appspot.com',
+      messagingSenderId: '208765891211',
+      appId: '1:208765891211:web:3d116d2fc4ef12a850da8e',
+      measurementId: 'G-JJDJQ9E9G4',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    // firebase.auth().onAuthStateChanged(user => {})
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
