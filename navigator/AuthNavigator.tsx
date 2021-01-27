b8eaaaf0ef5dbac2f722060a611a24181dff16d4
Marc Kalil <marc@marckalil.com>
2021-01-27 20:04:02 +0100

    Navigation header customised

--- navigator/AuthNavigator.tsx
+++ navigator/AuthNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {View} from 'react-native';
 import SignIn from '../views/SignIn';
 import SignUp from '../views/SignUp';
 
@@ -7,8 +8,37 @@ const AuthNavigator = (): React.ReactElement => {
   const {Navigator, Screen} = createStackNavigator();
   return (
     <Navigator>
-      <Screen name="SignIn" component={SignIn} />
-      <Screen name="SignUp" component={SignUp} />
+      <Screen
+        name="SignIn"
+        component={SignIn}
+        options={{
+          headerShown: false,
+          title: 'Sign in',
+        }}
+      />
+      <Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          title: 'Sign up',
+          headerStyle: {
+            backgroundColor: 'lightskyblue',
+          },
+          headerBackImage: ({tintColor}) => (
+            <View
+              style={{
+                height: 16,
+                aspectRatio: 1,
+                borderWidth: 2,
+                // borderColor: tintColor,
+                borderColor: 'white',
+                borderRadius: 8,
+                marginHorizontal: 10,
+              }}
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
